9b32242d1991b76890764ad9e00cb23823e47e5d
CheekonMTN <112905139+CheekonMTN@users.noreply.github.com>
2025-05-06 01:19:13 -0400

    finally done
    hallelujah (might need to get rid of junk files but that's all)

--- src/JFreePlusApache/JFAPlotter.java
+++ src/JFreePlusApache/JFAPlotter.java
@@ -10,14 +10,24 @@ import java.util.Random;
 import org.apache.commons.math4.legacy.stat.descriptive.DescriptiveStatistics;
 
 public class JFAPlotter {
+    public static XYSeries generatePureData() {
+        XYSeries data = new XYSeries("Pure Function");
+        for (double i = -3; i <= 3; i += 0.1) {
+            double x = i;
+            double y = Math.pow(x, 3) - 2 * x;
+            data.add(x, y);
+        }
+        return data;
+    }
+
     public static XYSeries generateNoisyData(int numPoints, double saltAmount) {
-        XYSeries data = new XYSeries("Original Data");
+        XYSeries data = new XYSeries("Noisy Data");
         Random rand = new Random();
 
-        for (int i = 0; i < numPoints; i++) {
+        for (double i = -3; i <= 3; i += 0.1) {
             double x = i;
-            double y = Math.sin(x/10.0) * 100; // base function
-            y += (rand.nextDouble() * 2 - 1) * saltAmount; // add noise
+            double y = Math.pow(x, 3) - 2 * x;
+            y += (rand.nextDouble() * 2 - 1) * saltAmount;
             data.add(x, y);
         }
         return data;
@@ -29,22 +39,19 @@ public class JFAPlotter {
 
         for (int i = 0; i < data.getItemCount(); i++) {
             stats.clear();
-
-            // Fill the window
             for (int j = Math.max(0, i - windowSize/2);
                  j < Math.min(data.getItemCount(), i + windowSize/2 + 1); j++) {
                 stats.addValue(data.getY(j).doubleValue());
             }
-
-            // Add smoothed point
             smoothed.add(data.getX(i), stats.getMean());
         }
         return smoothed;
     }
 
-    public static void plotData(XYSeries original, XYSeries smoothed) {
+    public static void plotData(XYSeries pure, XYSeries noisy, XYSeries smoothed) {
         XYSeriesCollection dataset = new XYSeriesCollection();
-        dataset.addSeries(original);
+        dataset.addSeries(pure);
+        dataset.addSeries(noisy);
         dataset.addSeries(smoothed);
 
         JFreeChart chart = ChartFactory.createXYLineChart(
@@ -56,8 +63,9 @@ public class JFAPlotter {
 
         XYPlot plot = (XYPlot) chart.getPlot();
         XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
-        renderer.setSeriesPaint(0, Color.BLUE);
-        renderer.setSeriesPaint(1, Color.RED);
+        renderer.setSeriesPaint(0, Color.GREEN);
+        renderer.setSeriesPaint(1, Color.BLUE);
+        renderer.setSeriesPaint(2, Color.RED);
         plot.setRenderer(renderer);
 
         ChartPanel chartPanel = new ChartPanel(chart);
@@ -70,8 +78,9 @@ public class JFAPlotter {
     }
 
     public static void main(String[] args) {
-        XYSeries original = generateNoisyData(100, 20);
-        XYSeries smoothed = smoothData(original, 15);
-        plotData(original, smoothed);
+        XYSeries pure = generatePureData();
+        XYSeries noisy = generateNoisyData(100, 1.0);
+        XYSeries smoothed = smoothData(noisy, 25);
+        plotData(pure, noisy, smoothed);
     }
 }
